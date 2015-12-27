fbe30ccc59f52fb46f11e1bc9b1a30d9df524546
Saurabh Datar <saurabhdatar@rescomp-15-320277.stanford.edu>
2015-12-27 15:51:53 -0800

    Flask app.py and first template

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	template = 'index.html'
+	return render_template(template)
+
+
+if __name__ == '__main__':
+	app.run(debug=True, use_reloader=True)
